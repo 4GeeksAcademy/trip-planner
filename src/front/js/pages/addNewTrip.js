57d5de0b746956071056eb565c187ea541f592be
Adriana Isea <adrianaisea02@gmail.com>
2024-10-04 18:01:38 +0000

    Cambio de diseño

--- src/front/js/pages/addNewTrip.js
+++ src/front/js/pages/addNewTrip.js
@@ -5,51 +5,90 @@ import { Link } from "react-router-dom";
 const NewTrip = () => {
     return (
         <div className="container mt-5">
-            <form className="p-4 bg-light rounded shadow w-50 mx-auto">
+            <form className="p-4 bg-dark  rounded shadow-lg w-50 mx-auto">
+
                 <div className="mb-3">
-                    <label htmlFor="destino" className="form-label">Destino</label>
-                    <input type="text" className="form-control" id="destino" placeholder="Ingresa el destino" required />
+                    <label htmlFor="destino" className="form-label text-light">Destino</label>
+                    <input
+                        type="text"
+                        className="form-control opacity-50 bg-light border-0 rounded-3"
+                        id="destino"
+                        placeholder="Ingresa el destino"
+                        required
+                    />
                 </div>
+
                 <div className="mb-3 row">
                     <div className="col">
-                        <label htmlFor="fechaInicio" className="form-label">Fecha Inicio</label>
-                        <input type="date" className="form-control" id="fechaInicio" required />
+                        <label htmlFor="fechaInicio" className="form-label text-light">Fecha Inicio</label>
+                        <input
+                            type="date"
+                            className="form-control opacity-50 bg-light  border-0 rounded-3"
+                            id="fechaInicio"
+                            required
+                        />
                     </div>
                     <div className="col">
-                        <label htmlFor="fechaFin" className="form-label">Fecha Fin</label>
-                        <input type="date" className="form-control" id="fechaFin" required />
+                        <label htmlFor="fechaFin" className="form-label text-light">Fecha Fin</label>
+                        <input
+                            type="date"
+                            className="form-control opacity-50 bg-light  border-0 rounded-3"
+                            id="fechaFin"
+                            required
+                        />
                     </div>
                 </div>
+
                 <div className="mb-3">
-                    <label htmlFor="presupuesto" className="form-label">Presupuesto</label>
-                    <input type="number" className="form-control" id="presupuesto" placeholder="Ingresa el monto estimado para tu viaje" />
+                    <label htmlFor="presupuesto" className="form-label text-light">Presupuesto</label>
+                    <input
+                        type="number"
+                        className="form-control opacity-50 bg-light  border-0 rounded-3"
+                        id="presupuesto"
+                        placeholder="Ingresa el monto estimado para tu viaje"
+                    />
                 </div>
+
                 <div className="mb-3">
-                    <label htmlFor="motivo" className="form-label">Motivo de Viaje</label>
-                    <textarea className="form-control" id="motivo" rows="3" placeholder="Ingresa el motivo del viaje (opcional)"></textarea>
+                    <label htmlFor="motivo" className="form-label text-light">Motivo de Viaje</label>
+                    <textarea
+                        className="form-control opacity-50 bg-light border-0 rounded-3"
+                        id="motivo"
+                        rows="1"
+                        placeholder="Ingresa el motivo del viaje (opcional)"
+                    ></textarea>
                 </div>
+
                 <div className="mb-3">
-                    <label htmlFor="nota" className="form-label">Nota</label>
-                    <textarea className="form-control" id="nota" rows="3" placeholder="Información importante para tu viaje (opcional)"></textarea>
+                    <label htmlFor="nota" className="form-label text-light">Nota</label>
+                    <textarea
+                        className="form-control opacity-50 bg-light border-0 rounded-3"
+                        id="nota"
+                        rows="2"
+                        placeholder="Información importante para tu viaje (opcional)"
+                    ></textarea>
                 </div>
 
                 <div className="mb-3 text-center">
-                    <div className="p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end rounded-4">
-                        <h5 className="text-dark">¡Invita a tus amigos!</h5>
-                        <input type="email" className="form-control" id="invitar" placeholder="Ingresa correo electrónico" />
+                    <div className="p-3 bg-opacity-10 border border-info rounded-3 text-light">
+                        <h5 className="text-ligth">¡Invita a tus amigos!</h5>
+                        <input
+                            type="email"
+                            className="form-control  opacity-50 bg-light  border-0 rounded-3"
+                            id="invitar"
+                            placeholder="Ingresa correo electrónico"
+                        />
                     </div>
                 </div>
 
                 <div className="mb-3">
-                    <button type="submit" className="btn btn-primary w-100 mb-5">¡Listo!</button>
-                    <div className="d-flex justify-content-start">
-                        <Link to="/" className="btn btn-secondary mt-2 d-flex align-items-center">
+                    <button type="submit" className="btn btn-primary w-100 mb-5 rounded-3">¡Listo!</button>
+                    <div className="d-flex justify-content-start opacity-50">
+                        <Link to="/" className="btn btn-secondary mt-2 d-flex align-items-center rounded-3">
                             <i className="fa-solid fa-circle-chevron-left me-2"></i> Inicio
                         </Link>
                     </div>
                 </div>
-
-
             </form>
         </div>
     );
