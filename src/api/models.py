7a16e255ed33b29fed7452622c5855fc1b014a7f
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-10-10 03:26:39 +0000

    primer borrador bases de datos, falta relacion muchos a muchos

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String(120), unique=False, nullable=False)
     username= db.Column(db.String(120), unique=True, nullable=False)
@@ -35,3 +37,47 @@ class User(db.Model):
             "more_info": self.more_info
             # do not serialize the password, its a security breach
         }
+
+class Viaje(db.Model):
+    __tablename__ = 'viajes'
+    id = db.Column(db.Integer, primary_key=True)
+    destino = db.Column(db.String(120), unique=False, nullable=False)
+
+class Grupo(db.Model):
+    __tablename__ = 'grupo'
+    id = db.Column(db.Integer, primary_key=True),
+    integrantes = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    viaje_id = db.Column(db.Integer, db.ForeignKey('grupo.id'))
+    viaje = db.relationship(Viaje)
+
+    # tabla de asociacion
+
+
+class Actividad(db.Model):
+    __tablename__ = 'actividad'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    precio = db.Column(db.Integer, unique=False, nullable=True)
+    moneda = db.Column(db.String(120), unique=False, nullable=True)
+    imagenes = db.Column(db.String(360), unique=False, nullable=False)
+    duracion = db.Column(db.Integer, unique=False, nullable=True)
+    viaje_id = db.Column(db.Integer, db.ForeignKey('viajes.id'))
+    viaje = db.relationship(Viaje)
+    descripcion = db.Column(db.String(360), unique=False, nullable=True)
+    likes =  db.Column(db.Integer, unique=False, nullable=True)
+    comentarios_id = db.Column(db.Integer, db.ForeignKey('comentarios.id'))
+
+
+class Comentarios(db.Model):
+     __tablename__ = 'commentario'
+     id = db.Column(db.Integer, primary_key=True)
+     activity_id = db.Column(db.Integer, db.ForeignKey('actividades.id'))
+     author_id = db.Column(db.Integer, db.ForeignKey('user.username'))
+     comentario = db.Column(db.String(120), unique=False, nullable=True)
+
+class Likes(db.Model):
+     __tablename__ = 'like'
+     id = db.Column(db.Integer, primary_key=True)
+     activity_id = db.Column(db.Integer, db.ForeignKey('actividades.id'))
+     author_id = Column(Integer, ForeignKey('user.username'))
