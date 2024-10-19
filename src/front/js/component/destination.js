a901e2da26940cd28b3a4cb647f8e55e5f78ef30
Adriana Isea <adrianaisea02@gmail.com>
2024-10-19 18:55:50 +0000

    cambios en la vista de destination

--- src/front/js/component/destination.js
+++ src/front/js/component/destination.js
@@ -20,35 +20,39 @@ export default function Destination() {
     return (
         <div>
             <div className="container-fluid mx-auto p-2">
-                <div className="d-flex justify-content-center mt-4" >
-                    <img id="destination" src={imagenCentral} className="img-fluid rounded rounded-3 shadow" style={{ width: "85%", height: "500px", objectFit: "cover" }} />
+                <div className="d-flex justify-content-center">
+                    <div className="position-relative" style={{ width: "80%", height: "250px" }}>
+                        <img id="destination" src={imagenCentral} className="img-fluid rounded rounded-3 shadow" style={{ width: "100%", height: "300px", objectFit: "cover" }} />
+                        <div className="overlay"></div>
+                        <p className="destinationDetail text-center fs-1 fw-bold my-3 text-white position-absolute top-50 start-50 translate-middle text-nowrap">{city}, {country}</p>
+                    </div>
                 </div>
             </div>
 
-            <div>
-                <p className="text-center fs-1 fw-bold my-3">{city}, {country}</p>
-            </div>
 
-            <div className="divider rounded-pill"></div>
 
-            <div className="activity-list">
+            <div className="activity-list d-flex flex-column align-items-center">
                 {data.map((item, index) => {
                     return (
-                        <div className="activity-card">
+                        <div className="activity-card" style={{ width: "80%", height: "170px" }}>
                             <div className="image-container">
-                                <img src={item.pictures[0]} alt="img activity" />
+                                <img src={item.pictures[0]} alt="img activity" className="rounded-top" />
                             </div>
-                            <div className="description">
+                            <div className="description p-4 d-flex flex-column">
                                 <h3>{item.name}</h3>
                                 <p>{item.shortDescription}</p>
                             </div>
-                           
-                                <button className="btn bg-secondary" onClick={() => handleAddActivity({name: 'Flyboard en Cancun'})}>
-                                    <Link to="/login" className="link"><i class="fa-solid fa-heart me-2 text-danger"></i> agregar</Link>
+
+                          
+                                <button className="btn fondoAzul shadow rounded-0 rounded-end" style={{ width: "7%" }} onClick={() => handleAddActivity({ name: 'Flyboard en Cancun' })}>
+                                    <Link to="/login" className="link" style={{ height: "100%" }}>
+                                    <i class="fa-solid fa-circle-plus me-2 fs-2 text-light"></i></Link>
                                 </button>
-                                </div>
-                            
-                    
+                          
+                        </div>
+
+
+
                     )
                 })}
             </div>
