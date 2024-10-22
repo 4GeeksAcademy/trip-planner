6b01d8f1602b482219e85a0162266fe45a4a6f0c
Adriana Isea <adrianaisea02@gmail.com>
2024-10-22 03:56:26 +0000

    Prueba 2 de agregar viaje

--- migrations/versions/3c38fc45bbcd_.py
+++ migrations/versions/3c38fc45bbcd_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 56f7599c8e2d
+Revision ID: 3c38fc45bbcd
 Revises: 
-Create Date: 2024-10-22 02:01:11.621514
+Create Date: 2024-10-22 03:21:39.395360
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '56f7599c8e2d'
+revision = '3c38fc45bbcd'
 down_revision = None
 branch_labels = None
 depends_on = None
