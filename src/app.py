680ebc015d6d3d31b617e3cfb428b1f993e55292
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-10-22 02:18:19 +0000

    cambios de Samuel

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask import Flask, request, jsonify, url_for, send_from_directory 
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
