e9e1c21d20f0bbc8fa4272c545de2b1f95c636ac
Adriana Isea <adrianaisea02@gmail.com>
2024-10-27 21:01:10 +0000

    Cambios en la vista de viajes (Loading y Nombre usuario)

--- src/front/js/pages/viajes.js
+++ src/front/js/pages/viajes.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric', timeZone: 'UTC' };
     return date.toLocaleDateString('es-ES', options);
 };
 
@@ -22,19 +22,19 @@ const Viajes = () => {
 
 
     useEffect(() => {
-        
-        const fetchData = async() =>{
-          setIsLoading(true)
-          try{
-              const data = await actions.get_trips();
-              setLongitudViaje(store.viajes.length)
-          } catch (error) {
-              console.error(error); // Handle any errors
+
+        const fetchData = async () => {
+            setIsLoading(true)
+            try {
+                const data = await actions.get_trips();
+                setLongitudViaje(store.viajes.length)
+            } catch (error) {
+                console.error(error); // Handle any errors
             } finally {
-              setIsLoading(false); // Finalizar la carga
-          }
-      }
-      fetchData()
+                setIsLoading(false); // Finalizar la carga
+            }
+        }
+        fetchData()
     }, [store.upDate]);
 
     const trips = store.viajes || [];
@@ -53,17 +53,17 @@ const Viajes = () => {
     const sortedTrips = trips.sort((a, b) => new Date(a.fecha_inicio) - new Date(b.fecha_inicio));
 
     console.log(store.viajes)
-    
+
     return (
         <>
             {/* User Banner */}
             {console.log(store.user)}
             <div className="d-flex flex-column flex-md-row align-items-center justify-content-center">
-            
+
                 <div className="container rounded d-flex bg-light p-4 shadow ms-5 me-3 mb-3 mb-md-0" style={{ maxWidth: "80%", width: "100%", marginTop: "20px" }}>
                     <div className="d-flex flex-column align-items-start flex-grow-1 mx-3" >
                         <h5>@{store.user.username}</h5>
-                        <p>{store.user.username}</p>
+                        <p>{store.user.name}</p>
                     </div>
                     <div className="mx-4 text-end">
                         <p className="mb-1"><i className="iconos fa-solid fa-map-location-dot me-2"></i>Proximos viajes: <span className="colorAzul fw-bold">{longitudViaje}</span></p>
@@ -73,47 +73,51 @@ const Viajes = () => {
                 </div>
 
                 <img src={store.user.profile_image_url || 'https://i.pinimg.com/550x/a8/0e/36/a80e3690318c08114011145fdcfa3ddb.jpg'} className="rounded-circle ms-0 me-5 shadow" style={{ objectFit: 'cover', width: "120px", height: "120px" }} />
-            
+
             </div>
 
 
 
             {/* individual trips*/}
-            {isLoading == true ? "Loading..." : 
-            
-            <div>
-            
-            {trips.map((item, index) => {
-                
-                // Para calcular los días transcurridos entre fecha y fecha en número entero
-                const fechaInicio = new Date(item.fecha_inicio);
-                const fechaFin = new Date(item.fecha_fin);
-                const dias = parseInt((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24), 10);
+            {isLoading == true ? <div class="text-center">
+                <div class="spinner-border m-5" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div> :
 
-                return (<div key={index} className="viaje container d-flex mb-3 my-5 rounded-pill p-2 bg-light " style={{ width: "100%", maxWidth: "65%" }}>
-                    <img src={item.trip_image_url || "https://firebasestorage.googleapis.com/v0/b/trippy-proyecto.appspot.com/o/fondoDestino.png?alt=media&token=c65fa4ed-494d-410b-bd9a-68e74ef3e456"} className="ima rounded-circle shadow" style={{ objectFit: 'cover', width: "100px", height: "100px" }} />
-                    <div className="mt-1 flex-grow-1">
-                        <h6 className="mb-2">{item.destino}</h6>
-                        <p className="mb-0 mt-3">{formatDate(item.fecha_inicio)} - {formatDate(item.fecha_fin)}</p>
-                        <p className="mb-0 mt-1">
-                            <i className="iconos fa-solid fa-clock me-2"></i>
-                            {dias >= 0 ? dias : 0} días
-                        </p>
-                    </div>
-                    <div className="d-flex flex-column justify-content-end ms-auto p-3">
-                        <p className="mb-0 fw-normal">Presupuesto: <span className="colorAzul fw-bold">$ {item.presupuesto || "No disponible"}</span></p>
-                        <p className="mb-0 fw-normal">Presupuesto personal: <span className="colorAzul fw-bold">$ {item.presupuesto_personal}</span></p>
-                        <p className="mb-0 fw-normal">Número de personas: <span className="colorAzul fw-bold">0</span></p>
-                    </div>
+                <div>
 
-                    <div className="d-flex align-items-center ms-3 me-3 fs-3">
-                        <Link to={`/trip/${item.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <i className="fa-solid fa-chevron-right opacity-50"></i>
-                        </Link>
-                    </div>
-                </div>)
-            })}
-            </div>
+                    {trips.map((item, index) => {
+
+                        // Para calcular los días transcurridos entre fecha y fecha en número entero
+                        const fechaInicio = new Date(item.fecha_inicio);
+                        const fechaFin = new Date(item.fecha_fin);
+                        const dias = parseInt((fechaFin - fechaInicio) / (1000 * 60 * 60 * 24), 10);
+
+                        return (<div key={index} className="viaje container d-flex mb-3 my-5 rounded-pill p-2 bg-light " style={{ width: "100%", maxWidth: "65%" }}>
+                            <img src={item.trip_image_url || "https://firebasestorage.googleapis.com/v0/b/trippy-proyecto.appspot.com/o/fondoDestino.png?alt=media&token=c65fa4ed-494d-410b-bd9a-68e74ef3e456"} className="ima rounded-circle shadow" style={{ objectFit: 'cover', width: "100px", height: "100px" }} />
+                            <div className="mt-1 flex-grow-1">
+                                <h6 className="mb-2">{item.destino}</h6>
+                                <p className="mb-0 mt-3">{formatDate(item.fecha_inicio)} - {formatDate(item.fecha_fin)}</p>
+                                <p className="mb-0 mt-1">
+                                    <i className="iconos fa-solid fa-clock me-2"></i>
+                                    {dias >= 0 ? dias : 0} días
+                                </p>
+                            </div>
+                            <div className="d-flex flex-column justify-content-end ms-auto p-3">
+                                <p className="mb-0 fw-normal">Presupuesto: <span className="colorAzul fw-bold">$ {item.presupuesto || "No disponible"}</span></p>
+                                <p className="mb-0 fw-normal">Presupuesto personal: <span className="colorAzul fw-bold">$ {item.presupuesto_personal}</span></p>
+                                <p className="mb-0 fw-normal">Número de personas: <span className="colorAzul fw-bold">0</span></p>
+                            </div>
+
+                            <div className="d-flex align-items-center ms-3 me-3 fs-3">
+                                <Link to={`/trip/${item.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <i className="fa-solid fa-chevron-right opacity-50"></i>
+                                </Link>
+                            </div>
+                        </div>)
+                    })}
+                </div>
             }
 
             <div className="d-flex justify-content-evenly">
