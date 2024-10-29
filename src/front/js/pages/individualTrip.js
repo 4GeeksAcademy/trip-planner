56d3eb88effeeee87a997e016159c596f5ca9110
Carlos Alzate <158311146+cdalzate91@users.noreply.github.com>
2024-10-29 17:13:55 +0000

    Borrador de Likes

--- src/front/js/pages/individualTrip.js
+++ src/front/js/pages/individualTrip.js
@@ -53,19 +53,9 @@ const IndividualTrip = () => {
         </div>;
     }
 
-    useEffect(() => {
-        if (loading) {
-            return <div className="text-center">
-                <div className="spinner-border m-5" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
-            </div>;
-        }
-
-        if (!viaje) {
-            return <p>Viaje no encontrado</p>;
-        }
-    });
+    if (!viaje) {
+        return <p>Viaje no encontrado</p>;
+    }
 
 
     const handleInputChange = (e) => {
@@ -224,7 +214,7 @@ const IndividualTrip = () => {
                     <AddActivity viajeId={viaje.id} />
 
                     {/* CARDS */}
-
+                    
                     {store.activities.map((item, index) => {
                         return (
 
@@ -236,9 +226,9 @@ const IndividualTrip = () => {
                                             <h5 className="card-title mb-0">{item.nombre_actividad}</h5>
                                         </div>
                                         <p className="card-text">{item.descripcion}</p>
-                                        <div className="d-flex justify-content-end align-items-center border-black border-bottom border-3  my-2">
-                                            <p className="p-2 rounded">${item.precio}</p>
-                                        </div>
+                                            <div className="d-flex justify-content-end align-items-center border-black border-bottom border-3  my-2">
+                                                <p className="p-2 rounded">${item.precio}</p>
+                                            </div>
                                         <div className="d-flex justify-content-between align-items-center mt-auto">
                                             <Link to="/details" state={item} className="detalles text-light btn-sm px-4">Ver más</Link>
                                             <div className="d-flex align-items-center">
@@ -252,15 +242,16 @@ const IndividualTrip = () => {
                                                         <i className={`text-danger fa-solid fa-heart me-2`}></i>
                                                     </>
                                                 }
+                                                    {/* ${actions.addLike(index) ? "fa-solid" : "fa-regular"} */}
                                                     {item.likes}
                                                 </button>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="card-footer text-center bg-secondary text-light p-2">
-                                        <button className={`btn btn btn-light btn-sm px-4 shadow ${actions.isViaje({ name: item.name, id: item.id, type: "tripDetail" }) && "btn-danger"} `}
-                                            onClick={() => actions.addViaje({ name: item.nombre_actividad, id: item.id, type: "tripDetail", cost: item.precio, imagenes: item.imagenes })}>
-                                            {actions.isViaje({ name: item.nombre_actividad, id: item.id, type: "tripDetail", cost: item.precio, imagenes: item.imagenes }) ?
+                                        <div className="card-footer text-center bg-secondary text-light p-2">
+                                            <button className={`btn btn btn-light btn-sm px-4 shadow ${actions.isViaje({name: item.name, id: item.id, type: "tripDetail"}) && "btn-danger"} `}
+                                            onClick={() => actions.addViaje( {name: item.nombre_actividad, id: item.id, type: "tripDetail", cost: item.precio, imagenes: item.imagenes} )}>
+                                            {actions.isViaje( {name: item.nombre_actividad, id: item.id, type: "tripDetail", cost: item.precio, imagenes: item.imagenes}) ? 
                                                 <>
                                                     <i className="text-danger delete-trip fa-solid fa-trash-can p-1"></i>
                                                     <span className="text-danger">Quitar</span>
@@ -278,13 +269,11 @@ const IndividualTrip = () => {
                         )
                     })}
                 </div>
-
+               
             </div>
         </div>
     )
-
-};
-
+}
 
 export default IndividualTrip;
 
