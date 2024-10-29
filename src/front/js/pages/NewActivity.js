77844668088d717714886d16d545e3c56d98fa13
Adriana Isea <adrianaisea02@gmail.com>
2024-10-29 03:56:51 +0000

    dIV CORREGIDO

--- src/front/js/pages/NewActivity.js
+++ src/front/js/pages/NewActivity.js
@@ -36,7 +36,7 @@ const NewActivity = () => {
 
     const upload = async () => {
         const activityImageUrl = activity.imagenes ? await uploadImage(activity.imagenes) : null;
-        await actions.addActivity({...activity, imagenes: activityImageUrl});
+        await actions.addActivity({ ...activity, imagenes: activityImageUrl });
         console.log(activity);
         setActivity({
             name: "",
@@ -73,48 +73,6 @@ const NewActivity = () => {
             <div className="container">
                 <div className="p-4 bg-dark  rounded shadow-lg w-50 mx-auto">
 
-                <div className="mb-3">
-                    <label htmlFor="Actividad" className="form-label text-light fs-3">Nombre de la actividad </label>
-                    <input
-                        type="text"
-                        className="form-control opacity-50 bg-light border-0 rounded-3"
-                        id="name"
-                        placeholder="Heladería"
-                        required
-                        value={activity.name}
-                        onChange={
-                            (event) => setActivity({
-                                ...activity,
-                                name: event.target.value
-                            })
-                        }
-                    />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label text-light"> <i className="colorNaranja fa-solid fa-image me-2"></i>Sube una foto de referencia</label>
-                    <div className = "d-flex justify-content-center">
-                        <img
-                            src={activity.imagenes ? URL.createObjectURL(activity.imagenes) : activity.activityImageUrl || 'https://i.pinimg.com/550x/a8/0e/36/a80e3690318c08114011145fdcfa3ddb.jpg'}
-                            className="rounded-3 mx-auto"
-                            style={{ width: '150px', cursor: 'pointer' }}
-                            onClick={() => document.getElementById('imagenActividad').click()} // Activa el input de archivo
-                            />
-                    </div>
-                    <div className="mb-3 d-flex flex-column">
-                        <input
-                            id="imagenActividad"
-                            type="file"
-                            accept="image/*"
-                            className="form-control"
-                            style={{ display: 'none' }}
-                            onChange={(event) => {
-                                console.log("Ruta de las imagenes", event.target.files[0]);   
-                                setActivity({
-                                    ...activity,
-                                    imagenes: event.target.files[0],
-                                });
-                            }}
-                            
                     <div className="mb-3">
                         <label htmlFor="Actividad" className="form-label text-light fs-3">Nombre de la actividad </label>
                         <input
@@ -149,81 +107,125 @@ const NewActivity = () => {
                                 accept="image/*"
                                 className="form-control"
                                 style={{ display: 'none' }}
-                                onChange={(event) => setActivity({
-                                    ...activity,
-                                    imagenes: event.target.files[0]
-                                })}
-                            />
+                                onChange={(event) => {
+                                    console.log("Ruta de las imagenes", event.target.files[0]);
+                                    setActivity({
+                                        ...activity,
+                                        imagenes: event.target.files[0],
+                                    });
+                                }}
+                            
+                    <div className="mb-3">
+                                <label htmlFor="Actividad" className="form-label text-light fs-3">Nombre de la actividad </label>
+                                <input
+                                    type="text"
+                                    className="form-control opacity-50 bg-light border-0 rounded-3"
+                                    id="name"
+                                    placeholder="Heladería"
+                                    required
+                                    value={activity.name}
+                                    onChange={
+                                        (event) => setActivity({
+                                            ...activity,
+                                            name: event.target.value
+                                        })
+                                    }
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label text-light"> <i className="colorNaranja fa-solid fa-image me-2"></i>Sube una foto de referencia</label>
+                                <div className="d-flex justify-content-center">
+                                    <img
+                                        src={activity.imagenes ? URL.createObjectURL(activity.imagenes) : activity.activityImageUrl || 'https://i.pinimg.com/550x/a8/0e/36/a80e3690318c08114011145fdcfa3ddb.jpg'}
+                                        className="rounded-3 mx-auto"
+                                        style={{ width: '150px', cursor: 'pointer' }}
+                                        onClick={() => document.getElementById('imagenActividad').click()} // Activa el input de archivo
+                                    />
+                                </div>
+                                <div className="mb-3 d-flex flex-column">
+                                    <input
+                                        id="imagenActividad"
+                                        type="file"
+                                        accept="image/*"
+                                        className="form-control"
+                                        style={{ display: 'none' }}
+                                        onChange={(event) => setActivity({
+                                            ...activity,
+                                            imagenes: event.target.files[0]
+                                        })}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="descripción" className="form-label text-light"> <i className="colorNaranja fa-solid fa-comment-dots me-2"></i>Descripción</label>
+                                <textarea
+                                    className="form-control opacity-50 bg-light border-0 rounded-3"
+                                    id="descripcion"
+                                    rows="2"
+                                    placeholder="Describe la actividad, horarios, datos importantes, entre otros..."
+                                    value={activity.descripcion}
+                                    onChange={
+                                        (event) => setActivity({
+                                            ...activity,
+                                            descripcion: event.target.value
+                                        })
+                                    }
+                                ></textarea>
+
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="Monto" className="form-label text-light"> <i className="colorNaranja fa-solid fa-sack-dollar me-2"></i>Costo apróximado</label>
+                                <textarea
+                                    className="form-control opacity-50 bg-light border-0 rounded-3"
+                                    id="precio"
+                                    rows="1"
+                                    placeholder="Ingresa el monto total que estimas para la actividad (opcional)"
+                                    value={activity.precio}
+                                    onChange={
+                                        (event) => setActivity({
+                                            ...activity,
+                                            precio: event.target.value
+                                        })
+                                    }
+                                ></textarea>
+                            </div>
+
+
+                            <div className="mb-3">
+                                <label htmlFor="duracion" className="form-label text-light"> <i className="colorNaranja fa-solid fa-clock me-2"></i>Duración de la actividad</label>
+                                <textarea
+                                    className="form-control opacity-50 bg-light border-0 rounded-3"
+                                    id="duracion"
+                                    rows="1"
+                                    placeholder="(opcional)"
+                                    value={activity.duracion}
+                                    onChange={
+                                        (event) => {
+                                            const newDuration = event.target.value;
+                                            console.log("Duración actualizada", newDuration);
+                                            setActivity(prevActivity => ({
+                                                ...prevActivity,
+                                                duracion: newDuration
+                                            }));
+                                        }}
+                                ></textarea>
+                            </div>
+
+
+                            <button onClick={upload} className="mb-3 btn btn-success w-100">Guardar</button>
+
+                            <div className="d-flex justify-content-start">
+                                <Link to="/"
+                                    className="btn btn-secondary mt-2 d-flex align-items-center rounded-3"
+                                >
+                                    <i className="fa-solid fa-circle-chevron-left me-2"></i> Inicio
+                                </Link>
+                            </div>
+
                         </div>
                     </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="descripción" className="form-label text-light"> <i className="colorNaranja fa-solid fa-comment-dots me-2"></i>Descripción</label>
-                        <textarea
-                            className="form-control opacity-50 bg-light border-0 rounded-3"
-                            id="descripcion"
-                            rows="2"
-                            placeholder="Describe la actividad, horarios, datos importantes, entre otros..."
-                            value={activity.descripcion}
-                            onChange={
-                                (event) => setActivity({
-                                    ...activity,
-                                    descripcion: event.target.value
-                                })
-                            }
-                        ></textarea>
-
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="Monto" className="form-label text-light"> <i className="colorNaranja fa-solid fa-sack-dollar me-2"></i>Costo apróximado</label>
-                        <textarea
-                            className="form-control opacity-50 bg-light border-0 rounded-3"
-                            id="precio"
-                            rows="1"
-                            placeholder="Ingresa el monto total que estimas para la actividad (opcional)"
-                            value={activity.precio}
-                            onChange={
-                                (event) => setActivity({
-                                    ...activity,
-                                    precio: event.target.value
-                                })
-                            }
-                        ></textarea>
-                    </div>
-
-
-                    <div className="mb-3">
-                        <label htmlFor="duracion" className="form-label text-light"> <i className="colorNaranja fa-solid fa-clock me-2"></i>Duración de la actividad</label>
-                        <textarea
-                            className="form-control opacity-50 bg-light border-0 rounded-3"
-                            id="duracion"
-                            rows="1"
-                            placeholder="(opcional)"
-                            value={activity.duracion}
-                            onChange={
-                                (event) => {
-                                    const newDuration = event.target.value;
-                                    console.log("Duración actualizada", newDuration);
-                                    setActivity(prevActivity => ({
-                                        ...prevActivity,
-                                        duracion: newDuration
-                                    }));
-                                }}
-                        ></textarea>
-                    </div>
-
-
-                    <button onClick={upload} className="mb-3 btn btn-success w-100">Guardar</button>
-
-                    <div className="d-flex justify-content-start">
-                        <Link to="/"
-                            className="btn btn-secondary mt-2 d-flex align-items-center rounded-3"
-                        >
-                            <i className="fa-solid fa-circle-chevron-left me-2"></i> Inicio
-                        </Link>
-                    </div>
-
                 </div>
             </div>
 
