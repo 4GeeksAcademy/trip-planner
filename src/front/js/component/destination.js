cf03bb2a8c10df07d767a1724314af7ad98b0d63
Adriana Isea <adrianaisea02@gmail.com>
2024-11-01 16:47:56 +0000

    Cambio de estilos y se agrega modal de advertencia para eliminar usuario

--- src/front/js/component/destination.js
+++ src/front/js/component/destination.js
@@ -36,17 +36,23 @@ export default function Destination() {
                     return (
                         <div className="activity-card" style={{ width: "80%", height: "170px" }}>
                             <div className="image-container">
-                                <img src={item.pictures[0]} alt="img activity" className="rounded-top" />
+                                <img src={item.pictures[0] || 'https://firebasestorage.googleapis.com/v0/b/trippy-proyecto.appspot.com/o/Dise%C3%B1o%20sin%20t%C3%ADtulo%20(14).png?alt=media&token=2703929a-4977-498e-91de-0ab70b68d609'} alt="img activity" className="rounded-top" 
+                                onError={(e) => { 
+                                    e.target.onerror = null; // Evita bucles infinitos
+                                    e.target.src = 'https://firebasestorage.googleapis.com/v0/b/trippy-proyecto.appspot.com/o/Dise%C3%B1o%20sin%20t%C3%ADtulo%20(14).png?alt=media&token=2703929a-4977-498e-91de-0ab70b68d609'; // Imagen por defecto
+                                }} 
+                                />
                             </div>
-                            <div className="description p-4 d-flex flex-column" style={{maxWidth: "700px"}}>
-                                <h3>{item.name}</h3>
-                                <p>{item.shortDescription}</p>
+                            <div className="description px-3 py-2 flex-column" style={{maxWidth: "700px"}}>
+                            <h3 className="text-truncate pt-1" style={{ maxWidth: "500px" }}>{item.name}</h3>
+                            <p style={{ margin: 0, whiteSpace: "normal" }}>
+                            {item.shortDescription}</p>
                             </div>
 
                           
-                                <button className="btn fondoAzul shadow rounded-0 rounded-end" style={{ width: "7%" }} onClick={() => handleAddActivity({ name: 'Flyboard en Cancun' })}>
+                                {/* <button className="btn fondoAzul shadow rounded-0 rounded-end" style={{ width: "7%" }} onClick={() => handleAddActivity({ name: 'Flyboard en Cancun' })}>
                                     <Link to="/login" className="link" style={{ height: "100%" }}></Link>
-                                </button>
+                                </button> */}
                           
                         </div>
 
