72666efd1186088324a5df7795ee0c1eb84dc3fe
Adriana Isea <adrianaisea02@gmail.com>
2024-11-01 17:15:01 +0000

    Estilos de descripción de actividades

--- src/front/js/pages/viajes.js
+++ src/front/js/pages/viajes.js
@@ -115,7 +115,6 @@ const Viajes = () => {
                             <div className="d-flex flex-column justify-content-end ms-auto px-3 py-2 mb-1">
                                 <p className="mb-0 fw-normal">Presupuesto: <span className="colorAzul fw-bold">$ {item.presupuesto || "0"}</span></p>
                                 <p className="mb-0 fw-normal">Presupuesto personal: <span className="colorAzul fw-bold">$ {item.presupuesto_personal}</span></p>
-                                <p className="mb-0 fw-normal">Número de personas: <span className="colorAzul fw-bold">0</span></p>
                                 <button 
                                 className=" btn btn-sm fondoNaranja mt-1 text-center delete-button rounded-pill"
                                 onClick={() => handleDelete(item.id)}
